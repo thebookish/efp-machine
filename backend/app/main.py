1df6f7108457ff0acc816d8afcf265ad76c4b2c7
thebookish <87561302+thebookish@users.noreply.github.com>
2025-09-04 00:52:23 +0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,15 @@ from app.models import Base
 import asyncio
 
 app = FastAPI(title="EFP Machine API", version="0.1.0")
-
+origins = [
+    "http://localhost:5173",  # Vite dev server
+    "http://localhost:3000",
+    "https://efp-machine-3.onrender.com",  # your deployed frontend URL
+]
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.ALLOW_ORIGINS],
+     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
