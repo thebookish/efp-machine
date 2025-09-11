86ff606c902b39e708ed87de035001b08078397c
thebookish <abulhasannabil@gmail.com>
2025-09-11 18:41:31 +0600

    fix

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -5,38 +5,38 @@ from typing import Optional
 
 
 class OrderCreate(BaseModel):
-    content_event_eventId: Optional[str]
-    content_event_messages_0_message: str
-    content_event_messages_0_timestamp: Optional[str]
-    content_event_messages_0_sender_uuid: Optional[str]
-    requester_uuid: Optional[str]
-    eurexContractCode: Optional[str]
-    expiryDate: Optional[str]
-    contractISIN: Optional[str]
-    primaryAssetClass: Optional[str]
-    baseProduct: Optional[str]
-    subProduct: Optional[str]
-    eurexProductISIN: Optional[str]
-    underlyingIndex: Optional[str]
-    underlyingIndexISIN: Optional[str]
-    currency: Optional[str]
-    strategyID: Optional[str]
-    strategyDescription: Optional[str]
-    tradeableId: Optional[str]
-    contractId: Optional[str]
-    contractName: Optional[str]
-    strategyID_1: Optional[str]
-    strategyDisplayName: Optional[str]
-    strategyBrandName: Optional[str]
-    orderType: Optional[str]
-    orderID: Optional[str]
-    state: Optional[str]
-    buySell: Optional[str]
-    price: Optional[float]
-    basis: Optional[float]
-    linkedOrderID: Optional[str]
-    refInstrument: Optional[str]
-    refPrice: Optional[float]
+    content_event_eventId: Optional[str] = None
+    content_event_messages_0_message: Optional[str] = None
+    content_event_messages_0_timestamp: Optional[str] = None
+    content_event_messages_0_sender_uuid: Optional[str] = None
+    requester_uuid: Optional[str] = None
+    eurexContractCode: Optional[str] = None
+    expiryDate: Optional[str] = None
+    contractISIN: Optional[str] = None
+    primaryAssetClass: Optional[str] = None
+    baseProduct: Optional[str] = None
+    subProduct: Optional[str] = None
+    eurexProductISIN: Optional[str] = None
+    underlyingIndex: Optional[str] = None
+    underlyingIndexISIN: Optional[str] = None
+    currency: Optional[str] = None
+    strategyID: Optional[str] = None
+    strategyDescription: Optional[str] = None
+    tradeableId: Optional[str] = None
+    contractId: Optional[str] = None
+    contractName: Optional[str] = None
+    strategyID_1: Optional[str] = None
+    strategyDisplayName: Optional[str] = None
+    strategyBrandName: Optional[str] = None
+    orderType: Optional[str] = None
+    orderID: Optional[str] = None
+    state: Optional[str] = None
+    buySell: Optional[str] = None
+    price: Optional[float] = None
+    basis: Optional[float] = None
+    linkedOrderID: Optional[str] = None
+    refInstrument: Optional[str] = None
+    refPrice: Optional[float] = None
 
     response: Optional[str] = None
     timestamp: Optional[datetime] = None
