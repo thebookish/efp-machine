ac39ffefff93ee08fd60a6a696b0e2b19e1bd31a
thebookish <abulhasannabil@gmail.com>
2025-09-18 15:11:13 +0600

    change db to postgresql

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     ENV: str = Field(default='production')
     LOG_LEVEL: str = Field(default='INFO')
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    DATABASE_URL: str = Field(default='sqlite+aiosqlite:///./efp.db')
+    DATABASE_URL: str = Field(default='postgresql+asyncpg://neondb_owner:npg_kaERqV3XBD6s@ep-frosty-voice-ad7jm1zk-pooler.c-2.us-east-1.aws.neon.tech/efp')
     ALLOW_ORIGINS: str = Field(default='http://localhost:5173')
     TIMEZONE_UK: str = Field(default='Europe/London')
     class Config:
--- backend/app/deps.py
+++ backend/app/deps.py
@@ -1,7 +1,9 @@
 # app/deps.py
+import ssl
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import make_url
+from sqlalchemy.engine import URL
 from app.config import settings
 
 def to_async_url(url_str: str) -> str:
@@ -12,13 +14,23 @@ def to_async_url(url_str: str) -> str:
     elif u.drivername.startswith("postgresql"):
         u = u.set(drivername="postgresql+asyncpg")
     return str(u)
-
-async_url = to_async_url(settings.DATABASE_URL)
+url = URL.create(
+    "postgresql+asyncpg",
+    username="neondb_owner",
+    password="npg_kaERqV3XBD6s",
+    host="ep-frosty-voice-ad7jm1zk-pooler.c-2.us-east-1.aws.neon.tech",
+    port=5432,
+    database="efp",
+)
+ssl_ctx = ssl.create_default_context()
+# async_url = to_async_url(settings.DATABASE_URL)
 
 # Bigger pool for bursts
 
 engine = create_async_engine(
-    async_url,
+    url,
+    # async_url,
+    connect_args={"ssl": ssl_ctx},
     pool_size=50,          # Number of persistent connections
     max_overflow=150,       # Temporary burst connections
     pool_timeout=30,       # Seconds to wait for a connection
